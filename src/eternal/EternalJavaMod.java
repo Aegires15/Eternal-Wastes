08c16045ca2aee8cabd5a9da0125219f1776038f
Aegires15 <billspc@myyahoo.com>
2025-06-19 18:20:20 -0500

    Update and rename ExampleJavaMod.java to EternalJavaMod.java

---
+++ src/eternal/EternalJavaMod.java
@@ -0,0 +1,21 @@
+package eternal;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class EternalJavaMod extends Mod{
+
+    public EternalJavaMod()
+    }
+
+    @Override
+    public void loadContent(){
+    }
+
+}
