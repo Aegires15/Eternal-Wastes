bc8f2292c4ef0d87838946001417c803359e29b1
Aegires15 <billspc@myyahoo.com>
2025-08-09 12:00:03 -0500

    Pyrois and Libra
    7 planets planned, testing by launching Pyrois

---
+++ src/eternal/content/EternalPlanets.java
@@ -0,0 +1,89 @@
+package eternal.content;
+
+import arc.func.*;
+import arc.graphics.*;
+import arc.math.*;
+import arc.math.geom.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.game.*;
+import mindustry.graphics.*;
+import mindustry.graphics.g3d.*;
+import mindustry.graphics.g3d.PlanetGrid.*;
+import mindustry.maps.planet.*;
+import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.meta.*;
+
+public class EternalPlanets{
+  public static Planet
+  libra,
+  pyrois;
+
+public static void load(){
+  libra = new Planet("libra", null, 4f, 14){{
+            bloom = true;
+            accessible = false;
+
+            meshLoader = () -> new SunMesh(
+                this, 4,
+                5, 0.3, 1.7, 1.2, 1,
+                1.1f,
+                Color.valueOf("ff7a38"),
+                Color.valueOf("ff9638"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffc64c"),
+                Color.valueOf("ffe371"),
+                Color.valueOf("f4ee8e")
+            );
+        }};
+  pyrois = new Planet("pyrois", libra, 1f, 4){{
+            generator = new ErekirPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 5);
+            cloudMeshLoader = () -> new MultiMesh(
+                new HexSkyMesh(this, 2, 0.15f, 0.14f, 5, Color.valueOf("eba768").a(0.75f), 2, 0.42f, 1f, 0.43f),
+                new HexSkyMesh(this, 3, 0.6f, 0.15f, 5, Color.valueOf("eea293").a(0.75f), 2, 0.42f, 1.2f, 0.45f)
+            );
+            alwaysUnlocked = true;
+            landCloudColor = Color.valueOf("ed6542");
+            atmosphereColor = Color.valueOf("f07218");
+            defaultEnv = Env.scorching | Env.terrestrial;
+            startSector = 10;
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            tidalLock = true;
+            orbitSpacing = 2f;
+            totalRadius += 2.6f;
+            lightSrcTo = 0.5f;
+            lightDstFrom = 0.2f;
+            clearSectorOnLose = true;
+            defaultCore = Blocks.coreBastion;
+            iconColor = Color.valueOf("ff9266");
+            enemyBuildSpeedMultiplier = 0.4f;
+
+            //TODO disallowed for now
+            allowLaunchToNumbered = true;
+
+            //TODO SHOULD there be lighting?
+            updateLighting = false;
+
+            defaultAttributes.set(Attribute.heat, 1.2f);
+
+            ruleSetter = r -> {
+                r.waveTeam = Team.malis;
+                r.placeRangeCheck = false;
+                r.showSpawns = true;
+                r.fog = true;
+                r.staticFog = true;
+                r.lighting = false;
+                r.coreDestroyClear = true;
+                r.onlyDepositCore = true;
+            };
+            campaignRuleDefaults.fog = true;
+            campaignRuleDefaults.showSpawns = true;
+            campaignRuleDefaults.rtsAI = true;
+
+            unlockedOnLand.add(Blocks.coreBastion);
+        }};
+}
+}
