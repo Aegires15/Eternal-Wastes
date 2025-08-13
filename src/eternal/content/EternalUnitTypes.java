6422bd2a583008a6b69980cc317612a6e6bbc252
Aegires15 <billspc@myyahoo.com>
2025-08-13 16:50:40 -0500

    Alternate core unit Theta

--- src/eternal/content/EternalUnitTypes.java
+++ src/eternal/content/EternalUnitTypes.java
@@ -58,7 +58,7 @@ public class EternalUnitTypes{
       speed = 4;
       hitSize = 9f;
       mineWalls = true;
-      mineFloors = false;
+      mineFloor = false;
       mineTier = 3;
       flying = true;
         health = 250;
